7a1d26be5c228e02531cc8a1ade8b91ea61f4447
SteveGongoraL <stv.brayton.gongora@gmail.com>
2024-04-27 18:19:52 -0600

    Se modifico el script para que fuera mas util y realista

--- camion.py
+++ camion.py
@@ -1,8 +1,15 @@
-x=int(input("Dime el primer digito: "))
-y=int(input("Dime el segundo digito: "))
-z=int(input("Dime el tercer digito "))
+sumatoria_digitos = 0
+print ("\n-- Digitos de tu boleto de camion --")
 
-if (x + y + z == 21):
-    print("Te deben un beso" )
-else:
-    print("Sigue participando" )
+while True:
+    digito_boleto = input("Dime un digito de tu boleto: ")
+    if digito_boleto == "":
+        print(f"Programa terminado. \nLa suma fue de: {sumatoria_digitos}")
+        if (digito_boleto == 21):
+            print("Te deben un beso" )
+        else:
+            print("Sigue participando" )
+        break
+    else:
+        sumatoria_digitos += int(digito_boleto)
+        print("Para terminar deje el valor vacio.\n")
